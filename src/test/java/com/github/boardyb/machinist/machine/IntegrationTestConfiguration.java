e729c4b5f3b247204be4fef1b20a38a09fc04f27
Balazs Bordas <balazs.bord@gmail.com>
2020-07-20 16:35:31 +0200

    Used @Value annotation instead of Environment to get application properties in test configuration.

--- src/test/java/com/github/boardyb/machinist/machine/IntegrationTestConfiguration.java
+++ src/test/java/com/github/boardyb/machinist/machine/IntegrationTestConfiguration.java
@@ -1,10 +1,9 @@
 package com.github.boardyb.machinist.machine;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -23,16 +22,17 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 public class IntegrationTestConfiguration {
 
-    @Autowired
-    private Environment env;
+    @Value("${jdbc.driverClassName}")
+    private String driverClassName;
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
-        dataSource.setUsername(env.getProperty("jdbc.user"));
-        dataSource.setPassword(env.getProperty("jdbc.pass"));
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(jdbcUrl);
 
         return dataSource;
     }
