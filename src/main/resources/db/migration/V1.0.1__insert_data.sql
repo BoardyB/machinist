fb8e5673d29f2d0ab13003c6c972ce3c76f8776c
Balazs Bordas <balazs.bord@gmail.com>
2020-07-20 20:28:06 +0200

    Added test data inserting flyway migration.

---
+++ src/main/resources/db/migration/V1.0.1__insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO machinist.machine (id, created_at, updated_at, name, description, year_of_production, deleted)
+VALUES ('4ac1a5ac-fd94-4605-8175-34cad364816c', '2020-07-20 15:02:00.915', '2020-07-20 15:09:03.711', 'Milling machine',
+        'A milling machine is a manufacturing machine which uses rotary cutters to remove material by advancing a cutter into a work piece.',
+        2017, false);
+INSERT INTO machinist.machine (id, created_at, updated_at, name, description, year_of_production, deleted)
+VALUES ('ce3635bd-b17f-4337-8366-b9ce2e8d2c09', '2020-07-20 17:32:29.388', '2020-07-20 17:32:29.388', 'Drill press',
+        'A drill press is a style of drill that may be mounted on a stand or bolted to the floor or workbench.',
+        2002, false);
+INSERT INTO machinist.machine (id, created_at, updated_at, name, description, year_of_production, deleted)
+VALUES ('09f4bca8-215d-46eb-bcc6-2d87656b26dc', '2020-07-20 19:11:45.276', '2020-07-20 19:11:45.276', 'Lathe machine',
+        'A lathe is a machine tool that rotates a workpiece about an axis of rotation to perform various operations with tools that are applied to the workpiece to create an object with symmetry about that axis.',
+        2010, false);
